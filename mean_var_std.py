fbd668b5e6a0c9f1e128707169232e088db84827
kjenn12 <ayeongcha0320@gmail.com>
2025-11-28 12:39:46 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,11 @@ import numpy as np
 
 def calculate(list):
 
-
-
+    
+    try:
+        listSize = list > 9
+        print("list is correctly larger than 9")
+    except ValueError:
+        print("List must contain nine numbers.")
 
     return calculations
